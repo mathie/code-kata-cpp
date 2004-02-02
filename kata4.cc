6404eca411217f8c906c16bb4d66d8938e0e9321
Graeme Mathieson <mathie@woss.name>
2004-02-02 17:13:16 +0000

    * Implement new versions of the functions from parts one and two,   factoring out as much common code as possible.  Personally, I think   I've found a rather elegant way of consolidating the code. :-)
    git-svn-id: file:///Users/mathie/Development/old-svn/projects/code-kata/trunk@247 d5f202f0-b0d6-0310-8bd8-c586a511d7e9

--- kata4.cc
+++ kata4.cc
@@ -1,8 +1,13 @@
 // Code Kata 4: Data Munging
 //
-// $Id: kata4.cc,v 1.3 2004/02/02 16:37:11 mathie Exp $
+// $Id: kata4.cc,v 1.4 2004/02/02 17:13:16 mathie Exp $
 //
 // $Log: kata4.cc,v $
+// Revision 1.4  2004/02/02 17:13:16  mathie
+// * Implement new versions of the functions from parts one and two,
+//   factoring out as much common code as possible.  Personally, I think
+//   I've found a rather elegant way of consolidating the code. :-)
+//
 // Revision 1.3  2004/02/02 16:37:11  mathie
 // * Implement the soccer table parser and a test case to verify the
 // * result.
@@ -25,6 +30,7 @@
 #include <cstdlib>
 #include <limits>
 #include <string>
+#include <vector>
 
 using boost::unit_test_framework::test_suite;
 using namespace std;
@@ -116,6 +122,77 @@ string find_min_goal_diff()
   return min_team_name;
 }
 
+typedef vector<string> row;
+typedef vector<row> table;
+
+// Parse a file into a vector of vectors, each containing strings
+// corresponding to the individual cells.
+table parse_table(const string& file) 
+{
+  table t;
+  ifstream f(file.c_str());
+  string line;
+  
+  while(getline(f, line)) {
+    // If the line has no non-whitespace characters or its first
+    // non-whitespace character isn't a digit, ignore the line.
+    size_t beg_field = line.find_first_not_of(whitespace);
+    if(beg_field == string::npos || line.find_first_of(number) != beg_field) {
+      continue;
+    }
+    
+    row r;
+    size_t end_field;
+    
+    while(beg_field != string::npos) {
+      end_field = line.find_first_of(whitespace, beg_field);
+      if (end_field != string::npos) {
+        r.push_back(string(line, beg_field, end_field - beg_field));
+      } else {
+        r.push_back(string(line, beg_field));
+        break;
+      }
+      beg_field = line.find_first_not_of(whitespace, end_field);
+    }
+    t.push_back(r);
+  }
+  return t;
+}
+
+// Given a table returned from parse_table(), along with the
+// zero-indexed result column and two columns to take the difference
+// between, return the intersection of the result column and the row
+// which contains the minimum difference between the two other columns.
+// Simpler to code than explain, apparently. :-)
+string find_min_diff(table& t, int res_col, int col_a, int col_b)
+{
+  string cur_min_str;
+  int cur_min_val = numeric_limits<int>::max();
+  
+  for(table::const_iterator row = t.begin(); row != t.end(); row++) {
+    int a = atoi((*row)[col_a].c_str());
+    int b = atoi((*row)[col_b].c_str());
+    int diff = abs(a - b);
+    if(diff < cur_min_val) {
+      cur_min_val = diff;
+      cur_min_str = (*row)[res_col];
+    }
+  }
+  return cur_min_str;
+}
+
+unsigned int find_min_spread_day_common()
+{
+  table t = parse_table("K4Weather.txt");
+  return atoi(find_min_diff(t, 0, 1, 2).c_str());
+}
+
+string find_min_goal_diff_common()
+{
+  table t = parse_table("K4Soccer.txt");
+  return find_min_diff(t, 1, 6, 8);
+}
+
 void test_weather_data()
 {
   // The goal of this task is to find the smallest range of temperature
@@ -124,6 +201,7 @@ void test_weather_data()
   //   cat K4Weather.txt|awk '{print $1, $2 - $3}'
   // and a little hand-filtering).
   BOOST_CHECK(find_min_spread_day() == 14);
+  BOOST_CHECK(find_min_spread_day_common() == 14);
 }
 
 void test_soccer_league_table()
@@ -138,6 +216,7 @@ void test_soccer_league_table()
   //
   // Do not ask what that odd whitespace character is all about...
   BOOST_CHECK(find_min_goal_diff() == "Aston\x5fVilla");
+  BOOST_CHECK(find_min_goal_diff_common() == "Aston\x5fVilla");
 }
 
 test_suite *init_unit_test_suite(int argc, char *argv[])
