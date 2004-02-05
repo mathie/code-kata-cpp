2b82231319b980a85693bb4b10c62e297d15f96d
Graeme Mathieson <mathie@woss.name>
2004-02-05 13:52:43 +0000

    * All use of an individual hash from the hash_list is typed in terms of   hash_list::value_type. * Generalise the testing functions. * Additional (disabled) trace in the md5_hash operator() function. * md5_hash now works for values (hash_bits % 8) != 0.
    git-svn-id: file:///Users/mathie/Development/old-svn/projects/code-kata/trunk@253 d5f202f0-b0d6-0310-8bd8-c586a511d7e9

--- kata5.cc
+++ kata5.cc
@@ -1,8 +1,15 @@
 // Code Kata 5: Bloom Filters
 //
-// $Id: kata5.cc,v 1.5 2004/02/05 06:04:00 mathie Exp $
+// $Id: kata5.cc,v 1.6 2004/02/05 13:52:43 mathie Exp $
 //
 // $Log: kata5.cc,v $
+// Revision 1.6  2004/02/05 13:52:43  mathie
+// * All use of an individual hash from the hash_list is typed in terms of
+//   hash_list::value_type.
+// * Generalise the testing functions.
+// * Additional (disabled) trace in the md5_hash operator() function.
+// * md5_hash now works for values (hash_bits % 8) != 0.
+//
 // Revision 1.5  2004/02/05 06:04:00  mathie
 // * Replace output messages in the test suite with BOOST_MESSAGE() so
 //   the verbosity of the test run is controlled by the test suite itself.
@@ -48,6 +55,7 @@
 #include <iostream>
 #include <fstream>
 #include <set>
+#include <iomanip>
 
 extern "C" {
 #include <openssl/md5.h>
@@ -58,11 +66,14 @@ using namespace std;
 
 const string dict = "/usr/share/dict/words";
 
-typedef vector<int> hash_list;
+typedef vector<unsigned int> hash_list;
 
-template <class hash_fn, int map_size = hash_fn::map_size>
+template <class hash_fn,
+          typename hash_list::value_type map_size = hash_fn::map_size>
 class bloom_filter 
 {
+  static const hash_list::value_type ms;
+  
   bitset<map_size> map;
   hash_fn hashes;
   
@@ -100,13 +111,28 @@ class bloom_filter
   {
     return (map.count() * 100) / map_size;
   }
-  
+
+  hash_list::value_type get_map_size() const
+  {
+    return ms;
+  }
+
+  const string& get_hash_name() const
+  {
+    return hashes.get_name();
+  }
 };
+
+template <class hash_fn,
+          typename hash_list::value_type map_size>
+const hash_list::value_type bloom_filter<hash_fn, map_size>::ms = map_size;
 
 class split_into_chars
 {
+  static const string name;
+  
  public:
-  static const int map_size;
+  static const hash_list::value_type map_size;
   
   hash_list operator()(const string& word) const
   {
@@ -116,37 +142,22 @@ class split_into_chars
     }
     return h;
   }
+  const string& get_name() const 
+  {
+    return name;
+  }
+  
 };
 
-const int split_into_chars::map_size = 256;
-
-void test_split_into_chars_hash()
-{
-  bloom_filter<split_into_chars> bf;
-  bf.insert("foo");
-  bf.insert("bar");
-  bf.insert("bazification");
-
-  BOOST_MESSAGE("bloom_filter<split_into_chars> with 3 entries is "
-                << bf.saturation() << "% full.");
-  
-  // Successful lookups
-  BOOST_CHECK(bf.lookup("foo") == true);
-  BOOST_CHECK(bf.lookup("bar") == true);
-  BOOST_CHECK(bf.lookup("bazification") == true);
-
-  // Try some things not in the dictionary
-  BOOST_CHECK(bf.lookup("notindict") == false);
-
-  // A false positive
-  BOOST_CHECK(bf.lookup("roof") == true);
-  
-}
+const hash_list::value_type split_into_chars::map_size = 256;
+const string split_into_chars::name = "split_into_chars";
 
 class char_pairs
 {
+  static const string name;
+  
  public:
-  static const int map_size;
+  static const hash_list::value_type map_size;
   
   hash_list operator()(const string& word) const
   {
@@ -160,103 +171,127 @@ class char_pairs
     }
     return h;
   }
+
+  const string& get_name() const 
+  {
+    return name;
+  }
 };
 
-const int char_pairs::map_size = 65536;
+const hash_list::value_type char_pairs::map_size = 65536;
+const string char_pairs::name = "char_pairs";
 
-void test_char_pairs_hash()
-{
-  bloom_filter<char_pairs> bf;
-  bf.insert("foo");
-  bf.insert("bars");
-  bf.insert("bazification");
-
-  BOOST_MESSAGE("bloom_filter<char_pairs> with 3 entries is "
-                << bf.saturation() << "% full.");
-  
-  // Successful lookups
-  BOOST_CHECK(bf.lookup("foo") == true);
-  BOOST_CHECK(bf.lookup("bars") == true);
-  BOOST_CHECK(bf.lookup("bazification") == true);
-
-  // Try some things not in the dictionary
-  BOOST_CHECK(bf.lookup("notindict") == false);
-
-  // A false positive
-  BOOST_CHECK(bf.lookup("rso") == true);
-  
-}
-
-template <size_t hash_bits = 16>
+template <hash_list::value_type hash_bits = 16>
 class md5_hash
 {
-  // TODO: This currently only works where (hash_bits % 8) == 0 and
-  // hash_bits < 32, mostly because of the for loop in operator() which
-  // turns the MD5 string into a list of appropriately-sized hashes.
-  BOOST_STATIC_ASSERT(hash_bits % 8 == 0);
-  BOOST_STATIC_ASSERT(hash_bits < 32);
-  
+  // TODO: This currently only works where hash_bits < sizeof(size_t) (the
+  // limit of the size of bitset<>).
+  BOOST_STATIC_ASSERT(hash_bits < (sizeof(size_t) << 3));
+
+  static const string name;
+
  public:
-  static const int map_size;
-  
+  static const hash_list::value_type map_size;
+
   hash_list operator()(const string& word) const
   {
-    unsigned char hash[16 + (hash_bits >> 3) + 1] = {0};
+    const unsigned int md5_len = 16; // bytes
+    unsigned char hash[md5_len + (hash_bits >> 3) + 1] = {0};
     MD5_CTX context;
     MD5_Init(&context);
     MD5_Update(&context, word.c_str(), word.size());
     MD5_Final(hash, &context);
 
 #if 0
-    clog << "hash = ";
+    clog << "hash = 0x";
     for(unsigned int i  = 0; i < 16; i++) {
-      clog << hex << static_cast<unsigned int>(hash[i]);
+      clog << setw(2) << setfill('0') << noshowbase << hex
+           << static_cast<unsigned int>(hash[i]);
     }
     clog << endl;
 #endif
 
     hash_list h;
-    for(unsigned int i = 0; i < 16; i += (hash_bits >> 3)) {
-      unsigned int v = 0;
-      for(unsigned int j = 0; j < (hash_bits >> 3); j++) {
-        v += hash[i + j] << (j << 3);
+    for(unsigned int start_bit = 0; start_bit < (md5_len << 3);
+        start_bit += hash_bits) {
+      const unsigned int start_byte = start_bit >> 3;
+      const unsigned int end_bit = start_bit + hash_bits - 1;
+      const unsigned int end_byte = end_bit >> 3;
+      const unsigned int end_byte_bits = (end_bit + 1) % 8;
+      
+      
+      hash_list::value_type v = 0;
+      for(unsigned int i = 0; i <= (end_byte - start_byte); i++) {
+        v += hash[end_byte - i] << (i << 3);
       }
+      v >>= end_byte_bits;
+      v &= (1 << hash_bits) - 1;
+      assert(v >= 0 && v < (1 << hash_bits));
+      
+#if 0
+      clog << "start_bit = " << dec << start_bit
+           << ", start_byte = " << dec << start_byte
+           << ", end_byte = " << dec << end_byte
+           << ", end_byte_bits = " << dec << end_byte_bits
+           << ", v = " << showbase << hex << v << endl;
+#endif
       h.push_back(v);
+    }
+
+    // There is a greater chance of the last element being zero,
+    // particularly if hash_bits causes the hash to be split in such a
+    // way that only one or two significant bits are included in that
+    // element.  To even things up a little, if it is zero, remove it.
+    if(h.back() == 0) {
+      h.pop_back();
     }
     
 #if 0
     clog << "hash_list = ";
     for(hash_list::const_iterator i = h.begin(); i != h.end(); i++) {
-      clog << hex << *i << ", ";
+      clog << setw((hash_bits >> 3) + 1) << setfill('0') << showbase
+           << hex << *i << ", ";
     }
     clog << endl;
 #endif
     
     return h;
   }
+
+  const string& get_name() const 
+  {
+    return name;
+  }
 };
 
-template <size_t hash_bits>
-const int md5_hash<hash_bits>::map_size = (1 << hash_bits);
+template <hash_list::value_type hash_bits>
+const hash_list::value_type md5_hash<hash_bits>::map_size = (1 << hash_bits);
+template <hash_list::value_type hash_bits>
+const string md5_hash<hash_bits>::name = "md5_hash";
 
-template <size_t hash_bits>
-void test_md5_hash()
+/////////////////
+// Test functions
+/////////////////
+
+template <class hash_fn>
+void test_insert_some_words()
 {
-  bloom_filter<md5_hash<hash_bits> > bf;
+  bloom_filter<hash_fn> bf;
   bf.insert("foo");
-  bf.insert("bars");
+  bf.insert("bar");
   bf.insert("bazification");
 
-  BOOST_MESSAGE("bloom_filter<md5_hash<" << hash_bits
-                << "> > with 3 entries is " << bf.saturation() << "% full.");
-
+  BOOST_MESSAGE(bf.get_hash_name() << " (map_size = " << bf.get_map_size()
+                << ") with 3 entries is "
+                << bf.saturation() << "% full.");
+  
   // Successful lookups
-  BOOST_CHECK_EQUAL(bf.lookup("foo"), true);
-  BOOST_CHECK_EQUAL(bf.lookup("bars"), true);
-  BOOST_CHECK_EQUAL(bf.lookup("bazification"), true);
+  BOOST_CHECK(bf.lookup("foo") == true);
+  BOOST_CHECK(bf.lookup("bar") == true);
+  BOOST_CHECK(bf.lookup("bazification") == true);
 
   // Try some things not in the dictionary
-  BOOST_CHECK_EQUAL(bf.lookup("notindict"), false);
+  BOOST_CHECK(bf.lookup("notindict") == false);
 }
 
 template <class hash_fn>
@@ -268,12 +303,13 @@ void test_dictionary()
   string word;
   unsigned int n_words = 0;
   while(getline(f, word)) {
-    BOOST_CHECK_EQUAL(bf.lookup(word), true); // No false negatives!
+    BOOST_CHECK(bf.lookup(word)); // No false negatives!
     n_words++;
   }
 
-  BOOST_MESSAGE(typeid(bf).name() << " with " << n_words
-                << " entries is " << bf.saturation() << "% full.");
+  BOOST_MESSAGE(bf.get_hash_name() << " (map_size = " << bf.get_map_size()
+                << ") with " << n_words << " entries is "
+                << bf.saturation() << "% full.");
 }
 
 template <size_t len>
@@ -290,9 +326,9 @@ string random_word()
 template<class hash_fn>
 void test_random_words()
 {
-  unsigned int pos = 0, neg = 0, fpos = 0;
-  const unsigned int tests = 10000;
+  const unsigned int n_tests = 10000;
   
+  unsigned int pos = 0, neg = 0, fpos = 0;
   bloom_filter<hash_fn> bf;
   bf.load_dictionary(dict);
 
@@ -304,7 +340,7 @@ void test_random_words()
   }
 
   
-  for(unsigned int i = 0; i < tests; i++) {
+  for(unsigned int i = 0; i < n_tests; i++) {
     string rword = random_word<5>();
     if(bf.lookup(rword)) {
       if(real_dict.find(rword) == real_dict.end()) {
@@ -319,7 +355,7 @@ void test_random_words()
   }
 
   BOOST_MESSAGE("With hash function " << typeid(hash_fn).name() << ",\n"
-                << "for " << tests << " test cases, there were "
+                << "for " << n_tests << " test cases, there were "
                 << neg << " words not in the dictionary,\n"
                 << pos << " words verified as being in the dictionary and "
                 << fpos << " false positives.");
@@ -330,14 +366,28 @@ test_suite *init_unit_test_suite(int argc, char *argv[])
   srand(time(NULL));
   
   test_suite *t = BOOST_TEST_SUITE("Code Kata 5: Bloom Filters");
-  t->add(BOOST_TEST_CASE(&test_split_into_chars_hash));
-  t->add(BOOST_TEST_CASE(&test_char_pairs_hash));
-  t->add(BOOST_TEST_CASE(&test_md5_hash<8>));
-  t->add(BOOST_TEST_CASE(&test_md5_hash<16>));
-  t->add(BOOST_TEST_CASE(&test_md5_hash<24>));
-  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<8> >));
-  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<16> >));
-  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<24> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<split_into_chars>));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<char_pairs>));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<8> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<16> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<17> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<18> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<19> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<20> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<21> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<22> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<23> >));
+  t->add(BOOST_TEST_CASE(&test_insert_some_words<md5_hash<24> >));
+  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<18> >));
+  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<19> >));
+  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<20> >));
+  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<21> >));
+  t->add(BOOST_TEST_CASE(&test_dictionary<md5_hash<22> >));
+  t->add(BOOST_TEST_CASE(&test_random_words<md5_hash<18> >));
+  t->add(BOOST_TEST_CASE(&test_random_words<md5_hash<19> >));
+  t->add(BOOST_TEST_CASE(&test_random_words<md5_hash<20> >));
+  t->add(BOOST_TEST_CASE(&test_random_words<md5_hash<21> >));
+  t->add(BOOST_TEST_CASE(&test_random_words<md5_hash<22> >));
   t->add(BOOST_TEST_CASE(&test_random_words<md5_hash<24> >));
   return t;
 }
